2a3b250ae5302bf1f69e1cdcd3e8bdc8c3d21cc1
LukeDasios <luke.dasios321@gmail.com>
2022-06-19 19:42:48 -0400

    Final commit, project done

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+require("dotenv").config()
 
+const express = require('express')
+const https = require('https')
+const bodyParser = require('body-parser')
 
+const API_KEY = process.env.API_KEY
 const app = express()
 
 // telling my app to use ejs as the default template engine
@@ -17,8 +22,8 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  const location = req.body.location ? req.body.location : "Purnia";
-  const appId = "Your_API_Key_Here";
+  const location = req.body.location ? req.body.location : "Ontario";
+  const appId = API_KEY;
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + appId + "&units=metric";
   https.get(url, (response) => {
     if (response.statusCode === 200) {
